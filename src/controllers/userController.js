615401893b060f7be5a087da1c154f0d5326f978
amankr1635 <amankr.1635@gmail.com>
2023-01-23 12:24:57 +0530

    2nd commit

---
+++ src/controllers/userController.js
@@ -0,0 +1,14 @@
+// - Create a user - atleast 5 users
+// - Create a user document from request body.
+// - Return HTTP status 201 on a succesful user creation. Also return the user document. The response should be a JSON object like [this](#successful-response-structure)
+// - Return HTTP status 400 if no params or invalid params received in request body. The response should be a JSON object like [this](#error-response-structure)
+
+
+const createUser = async function(req,res){
+    let body = req.body;
+    if(Object.keys(body).length == 0){
+        return res.status(400).send({status: false, message: "please enter some data in body" })
+    }
+    let createData = await userModel.create(body)
+    return res.status(201).send({status: true, message: "Suscessfully creeated", data: createData}) 
+}
--- src/routes/router.js
+++ src/routes/router.js
@@ -5,4 +5,11 @@ const router = express.Router
 
 
 
+
+
+
+router.all("/*",function(req,res){
+    return res.status(400).send({status: false, message: "invlalid http request"})
+} )
+
 module.exports = router
