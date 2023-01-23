ee7f2c72e8ab75afefbf21fd9c468e22d5f8323d
amankr1635 <amankr.1635@gmail.com>
2023-01-23 20:08:49 +0530

    changes on bookController

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,17 +1,8 @@
 const mongoose = require("mongoose");
 const bookModel = require("../models/bookModel");
 const userModel = require("../models/userModel");
-//const ISBN=require("isbn-validate")
 const reviewModel = require("../models/reviewModel");
-const moment = require("moment");
 const { regForDate, isValidString } = require("../validators/validation");
-let titleregex = /^[a-zA-Z_0-9? ,_-]{2,50}$/;
-//let titleregex = /^[A-Za-z0-9? ,_-]+$/;
-// - Create a book document from request body. Get userId in request body only.
-// - Make sure the userId is a valid userId by checking the user exist in the users collection.
-// - Return HTTP status 201 on a succesful book creation. Also return the book document. The response should be a JSON object like [this](#successful-response-structure)
-// - Create atleast 10 books for each user
-// - Return HTTP status 400 for an invalid request with a response body like [this](#error-response-structure)
 
 const createBook = async function (req, res) {
   let data = req.body;
@@ -47,6 +38,11 @@ const createBook = async function (req, res) {
       .status(400)
       .send({ status: false, message: "Must contain minimum 3 letters" });
 
+  if (!data.userId)
+    return res.status(400).send({ status: false, message: "Provide user ID" });
+  if (!mongoose.Types.ObjectId.isValid(userId))
+    return res.status(400).send({ status: false, msg: "user ID is incorrect" });
+
   if (!data.ISBN)
     return res.status(400).send({ status: false, message: "Provide ISBN" });
   if (!isValidString(data.ISBN))
@@ -63,11 +59,6 @@ const createBook = async function (req, res) {
     return res
       .status(400)
       .send({ status: false, message: "ISBN is already present" });
-
-  if (!userId)
-    return res.status(400).send({ status: false, message: "Provide user ID" });
-  if (!mongoose.Types.ObjectId.isValid(userId))
-    return res.status(400).send({ status: false, msg: "user ID is incorrect" });
 
   if (!data.category)
     return res.status(400).send({ status: false, message: "Provide category" });
@@ -90,27 +81,17 @@ const createBook = async function (req, res) {
       .status(400)
       .send({ status: false, message: "Provide released time" });
 
-  //if(!delete) return res.status(400).send({status:false,message:"Provide review"});
-
-  //let dateRegex=/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/
-
+      
+  if (regForDate(data.releasedAt) == false)
+  return res.status(400).send({
+    status: false,
+    message: "Please enter a valid date(YYYY-MM-DD)",
+  });
   let checkUser = await userModel.findById(userId);
   if (!checkUser)
     return res
       .status(404)
       .send({ status: false, message: "No documents found with this user ID" });
-
-  if (regForDate(data.releasedAt) == false)
-    return res
-      .status(400)
-      .send({
-        status: false,
-        message: "Please enter a valid date(YYYY-MM-DD)",
-      });
-  console.log(data.releasedAt);
-
-  //let reviewCount=await reviewModel.find().count();
-  //console.log(reviewCount);
 
   let created = await bookModel.create(data);
   return res
@@ -118,19 +99,35 @@ const createBook = async function (req, res) {
     .send({ status: true, message: "Success", data: created });
 };
 
-const getBooks = async function(req,res){
-    let books = req.query
-    if(Object.keys(books).length==0)return res.status(404).send({status:false,message:"Please enter some data"})
-    let bookData = await bookModel.find({...books,isDeleted:false}).select({title:1,excerpt:1,userId:1,category:1,releasedAt:1,reviews:1}).sort({title : 1})
-    
-    console.log(bookData);
+const getBooks = async function (req, res) {
+  let books = req.query;
+  if (Object.keys(books).length == 0)
+    return res
+      .status(404)
+      .send({ status: false, message: "Please enter some data" });
+  let bookData = await bookModel
+    .find({ ...books, isDeleted: false })
+    .select({
+      title: 1,
+      excerpt: 1,
+      userId: 1,
+      category: 1,
+      releasedAt: 1,
+      reviews: 1,
+    })
+    .sort({ title: 1 });
 
-    if(bookData.length==0){
-        return res.status(404).send({status:false,message:"no document found"})
-    }
-    return res.status(200).send({status:true,message:"success", data:bookData})
-}
+  console.log(bookData);
+
+  if (bookData.length == 0) {
+    return res
+      .status(404)
+      .send({ status: false, message: "no document found" });
+  }
+  return res
+    .status(200)
+    .send({ status: true, message: "success", data: bookData });
+};
 
 module.exports.createBook = createBook;
 module.exports.getBooks = getBooks;
-
